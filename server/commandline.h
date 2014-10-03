79d0c908a4c539acba88c5a6d52d301044e23df4
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-10-03 18:50:01 +0200

    Document the commandline parser
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- server/commandline.h
+++ server/commandline.h
@@ -9,7 +9,12 @@
 namespace traffic {
 namespace po = boost::program_options;
 
-
+/**
+ * \brief This handles all commandline parsing for the daemon.
+ *
+ * This uses boost::program_options to parse the commandline arguments
+ * of the program and provide them in a abstracted way.
+ */
 class Commandline
 {
 private:
@@ -20,8 +25,26 @@ private:
 	Commandline& operator=(Commandline const &) = delete;
 
 public:
+
+	/**
+	 * \brief Give the specifies bind addresses.
+	 *
+	 * This is a list of addresses the daemon should bind to to
+	 * listen for client connections.
+	 */
 	std::vector<std::string> addresses() const;
 
+	/**
+	 * \brief Parse the commandline arguments.
+	 *
+	 * This parses the argv array to get all the arguments. It
+	 * returns true if the parsing was successfull and the program
+	 * can proceed.
+	 *
+	 * \param argc The argument count.
+	 * \param argv The argv array.
+	 * \return true if the programm can proceed.
+	 */
 	bool parse(int argc, char const *argv[]);
 
 	Commandline();
