fa9aa97e4ef349c3aa869cacaedfb2801bf4e1e6
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-10-03 18:41:15 +0200

    Document the public interface of the TrafficServer
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- server/traffic_server.h
+++ server/traffic_server.h
@@ -18,9 +18,25 @@ namespace zmq {
 
 namespace traffic {
 
+
+/**
+ * \brief This handles the connections from the world.
+ *
+ * The TrafficServer handles the connections from the clients and proxy
+ * them fairly to the workers. It is also responsible for the socket
+ * setup and the creation of the worker threads.
+ */
 class TrafficServer
 {
 public:
+
+	/**
+	 * \brief This is the base for all workers.
+	 *
+	 * Implement this class in all worker implementations the
+	 * server can handle. It implements the message receiving and
+	 * sending.
+	 */
 	class WorkerBase
 	{
 	private:
@@ -31,6 +47,18 @@ public:
 		operator= (WorkerBase const &) = delete;
 
 	protected:
+
+		/**
+		 * \brief This is the "work" method.
+		 *
+		 * This method is called when a message was received.
+		 * Implement it to du the actual work. The result
+		 * should be stored in the result string.
+		 *
+		 * \param result The result of the processing.
+		 * \param data   The data from the received message
+		 * \param size   The size of the received message data.
+		 */
 		virtual bool process(std::string &result,
 				     void * data,
 				     size_t size) = 0;
@@ -50,9 +78,37 @@ private:
 	TrafficServer& operator= (TrafficServer const &) = delete;
 
 public:
+
+	/**
+	 * \brief bind the Server to the given addrress.
+	 *
+	 * This tells zeroMQ to bind the client socket to the given
+	 * address. The format of the address is specified in
+	 * zmq_tcp(7). This must be called before calling the run()
+	 * method.
+	 *
+	 * \param address The address to bind to.
+	 */
 	bool bind(std::string const &address);
+
+	/**
+	 * \brief Start a specific worker.
+	 *
+	 * This starts the given worker in its own thread. It calls the
+	 * run() method of the Worker to start the work loop. This
+	 * must be called befor the run() method of the Server.
+	 *
+	 * \param worker The worker to start.
+	 */
 	bool start_worker(WorkerBase * worker);
 
+	/**
+	 * \brief Start the server-IO.
+	 *
+	 * This starts the proxy between the incomming client
+	 * connections and the workers. You should have all bindings
+	 * done and workers running when calling this.
+	 */
 	void run();
 
 	TrafficServer();
