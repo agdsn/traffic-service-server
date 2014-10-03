b95978ce67a377b36634529cba3dc8bc8d3f4b1d
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-10-03 03:31:31 +0200

    Play around with gtest to test Commandline
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- test/server_commandline.cpp
+++ test/server_commandline.cpp
@@ -1,20 +1,65 @@
 #include <vector>
 #include <string>
+#include <iostream>
+#include <streambuf>
+#include <sstream>
 
 #include "gtest/gtest.h"
 
 #include "commandline.h"
 
 
-TEST(ServerCommandline, test_help) {
+class ServerCommandline : public ::testing::Test
+{
+private:
+	std::stringstream _std_out_stream;
+	std::stringstream _std_err_stream;
+	std::streambuf *_orig_out;
+	std::streambuf *_orig_err;
+
+public:
+	ServerCommandline()
+	:
+		_std_out_stream(),
+		_std_err_stream(),
+		_orig_out(std::cout.rdbuf()),
+		_orig_err(std::cerr.rdbuf())
+	{
+		std::cout.rdbuf(_std_out_stream.rdbuf());
+		std::cerr.rdbuf(_std_err_stream.rdbuf());
+	}
+
+	~ServerCommandline()
+	{
+		std::cout.rdbuf(_orig_out);
+		std::cerr.rdbuf(_orig_err);
+	}
+
+	std::string output() const
+	{
+		return _std_out_stream.str();
+	}
+
+	std::string error() const
+	{
+		return _std_err_stream.str();
+	}
+};
+
+
+// Test if -h prints help and exits
+TEST_F(ServerCommandline, test_help) {
 	traffic::Commandline cmd;
 	const char* argv[] = {"foobar", "-h"};
 
-	ASSERT_FALSE(cmd.parse(2, argv));
+	EXPECT_FALSE(cmd.parse(2, argv));
+
+	EXPECT_NE(output().find("Print help message."), std::string::npos);
 }
 
 
-TEST(ServerCommandline, test_bind_default) {
+// Test if default bind works
+TEST_F(ServerCommandline, test_bind_default) {
 	traffic::Commandline cmd;
 	const char* argv[] = {"foobar"};
 
@@ -26,7 +71,8 @@ TEST(ServerCommandline, test_bind_default) {
 }
 
 
-TEST(ServerCommandline, test_bind_single) {
+// Test if we can bind a single address
+TEST_F(ServerCommandline, test_bind_single) {
 	traffic::Commandline cmd;
 	const char* argv[] = {"foobar", "-b", "abcde"};
 
@@ -38,7 +84,8 @@ TEST(ServerCommandline, test_bind_single) {
 }
 
 
-TEST(ServerCommandline, test_bind_multi) {
+// Test if we can bind multiple addresses
+TEST_F(ServerCommandline, test_bind_multi) {
 	traffic::Commandline cmd;
 	const char* argv[] = {"foobar",
 		"-b", "abcde",
@@ -55,9 +102,11 @@ TEST(ServerCommandline, test_bind_multi) {
 }
 
 
-TEST(ServerCommandline, test_bind_missing) {
+// Test if we get an error if bind arg is omitted
+TEST_F(ServerCommandline, test_bind_missing) {
 	traffic::Commandline cmd;
 	const char* argv[] = {"foobar", "-b"};
 
-	ASSERT_FALSE(cmd.parse(2, argv));
+	EXPECT_FALSE(cmd.parse(2, argv));
+	EXPECT_NE(error().find("the required argument for option '--bind' is missing"), std::string::npos);
 }
