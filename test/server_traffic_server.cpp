6a4ace5c4af049dd622a7372571309afd088666c
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-10-04 01:06:55 +0200

    Add unittest with multiple workers
    This tests that the work is split across the workers.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- test/server_traffic_server.cpp
+++ test/server_traffic_server.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <thread>
 #include <unistd.h>
 #include <zmq.hpp>
@@ -24,10 +25,10 @@ public:
 		return true;
 	}
 
-	bool process(std::string& res, void*, size_t)
+	bool process(std::string& res, void*, size_t sz)
 	{
 		message = true;
-		res += "XXX";
+		res = std::string(sz, 'X');
 		return true;
 	}
 
@@ -46,7 +47,7 @@ TEST(ServerTrafficServer, test_bind_worker) {
 
 	std::thread tr([ & ] (){ server.run(); });
 
-	usleep(30); // Wari for the proxy() call
+	usleep(30); // Wait for the proxy() call
 
 	{
 		zmq::context_t ctx(1);
@@ -58,7 +59,7 @@ TEST(ServerTrafficServer, test_bind_worker) {
 
 		zmq::message_t res;
 		EXPECT_TRUE(sock.recv(&res));
-		EXPECT_EQ((size_t)3U, res.size());
+		EXPECT_EQ((size_t)10U, res.size());
 
 		sock.close();
 	}
@@ -66,4 +67,45 @@ TEST(ServerTrafficServer, test_bind_worker) {
 	server.stop();
 	tr.join();
 	EXPECT_TRUE(w.message);
+}
+
+
+// Test multiple worker message distribution
+TEST(ServerTrafficServer, test_multi_workers) {
+	std::vector<TestWorker> workers(3);
+
+	traffic::TrafficServer server;
+	server.bind(TEST_ADDRESS);
+
+	for (auto& worker : workers) {
+		server.start_worker( &worker );
+		EXPECT_TRUE(worker.started);
+	}
+
+	std::thread tr([ & ] (){ server.run(); });
+
+	usleep(30); // Wait for the proxy() call
+
+	for (size_t i = 0; i < workers.size(); ++i) {
+		zmq::context_t ctx(1);
+		zmq::socket_t sock(ctx, ZMQ_REQ);
+		sock.connect(TEST_ADDRESS);
+
+		zmq::message_t msg(10+i);
+		EXPECT_TRUE(sock.send(msg));
+
+		zmq::message_t res;
+		EXPECT_TRUE(sock.recv(&res));
+		EXPECT_EQ((size_t)(10U+i), res.size());
+
+		sock.close();
+		EXPECT_EQ((int)(i+1U),
+			   std::count_if(workers.begin(),
+					 workers.end(),
+					 [](TestWorker const &w)
+						{ return w.message; }));
+	}
+
+	server.stop();
+	tr.join();
 }
