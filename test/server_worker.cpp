442b85beeb6ac8617889eef0cc28b99c6957dd1f
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-10-05 21:24:19 +0200

    Add simple test for message prototype
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ test/server_worker.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.h"
+
+#include "worker.h"
+#include "requests.pb.h"
+
+
+class TestMessageWorker : public traffic::MessageWorker
+{
+protected:
+	bool set_up() { return true; }
+
+	bool process_summary() {
+		summary = true;
+		return true;
+	}
+
+	bool process_statistics() {
+		statistic = true;
+		return true;
+	}
+
+public:
+	bool statistic;
+	bool summary;
+
+	using traffic::MessageWorker::process;
+
+	TestMessageWorker()
+	:
+		traffic::MessageWorker(),
+		statistic(false),
+		summary(false)
+	{ }
+};
+
+
+TEST(ServerMessageWorker, basic_message_worker) {
+	TestMessageWorker w;
+
+	request::Request request;
+	request.set_version(1);
+
+	request::Statistic *stat = request.mutable_statistic();
+
+	request::TimeRange *range = stat->mutable_range();
+
+	range->set_start(1);
+	range->set_end(2);
+
+	stat->set_address("10.10.10.10");
+
+	std::string buffer;
+	request.SerializeToString(&buffer);
+
+	std::string out_buffer;
+	w.process(out_buffer, (void *)buffer.c_str(), buffer.size());
+
+	EXPECT_TRUE(w.statistic);
+	EXPECT_FALSE(w.summary);
+}
