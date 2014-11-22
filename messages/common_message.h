3dce21c59e1cbc04fa6c77ebc148f209d5abafc5
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-11-22 20:54:31 +0100

    Add some documentation
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- messages/common_message.h
+++ messages/common_message.h
@@ -6,6 +6,10 @@
 
 namespace traffic {
 
+
+/**
+ * \brief Wraps the TimeRange message type.
+ */
 class TimeRange
 {
 private:
@@ -13,8 +17,22 @@ private:
 	time_t const _end;
 
 public:
+	/**
+	 * \brief The start of the interval.
+	 */
 	time_t start() const { return _start; }
+
+	/**
+	 * \brief The end of the interval.
+	 */
 	time_t end() const { return _end; }
+
+	/**
+	 * \brief Tells if this is a valid interval.
+	 *
+	 * A valid interval is one where the start is before the
+	 * end timesoamp.
+	 */
 	bool valid() const { return _end > _start; }
 
 	TimeRange(time_t const start, time_t const end);
