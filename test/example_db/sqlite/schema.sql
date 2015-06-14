86d2e92760e596cf571863a854734c57b4cbed13
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-06-15 00:44:13 +0200

    Fix typo in sqlite schema
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- test/example_db/sqlite/schema.sql
+++ test/example_db/sqlite/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE inbound (
 	ip_dst VARCHAR NOT NULL,
 	packets UNSIGNED INTEGER DEFAULT 0,
 	bytes UNSIGNED BIG INTEGER DEFAULT 0,
-	stamp_inserted INTGER NOT NULL,
+	stamp_inserted INTEGER NOT NULL,
 	stamp_updated INTEGER,
 	CONSTRAINT inbound_pk PRIMARY KEY (ip_dst, stamp_inserted)
 );
@@ -11,7 +11,7 @@ CREATE TABLE outbound (
 	ip_src VARCHAR NOT NULL,
 	packets UNSIGNED INTEGER DEFAULT 0,
 	bytes UNSIGNED BIG INTEGER DEFAULT 0,
-	stamp_inserted INTGER NOT NULL,
+	stamp_inserted INTEGER NOT NULL,
 	stamp_updated INTEGER,
 	CONSTRAINT outbound_pk PRIMARY KEY (ip_src, stamp_inserted)
 );
