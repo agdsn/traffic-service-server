88652b330a52b1e9940657be5eec0a684bf117b7
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-06-17 01:18:11 +0200

    Add unittest for statistic requests
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- test/messages_request_message.cpp
+++ test/messages_request_message.cpp
@@ -50,7 +50,23 @@ std::string summary_message()
 }
 
 
-// Test that basic message serializing work
+std::string statistic_message()
+{
+	requests::Request req;
+	req.set_version(1);
+
+	requests::Statistic *statistic(req.mutable_statistic());
+	statistic->mutable_range()->set_start(1UL);
+	statistic->mutable_range()->set_end(2UL);
+
+	statistic->set_address("peng");
+	statistic->set_data_interval(requests::Statistic::HOUR);
+
+	return req.SerializeAsString();
+}
+
+
+// Test that summary message processing work
 TEST(MessagesRequest, deserialize_summary_request) {
 	std::string bytes(summary_message());
 
@@ -77,6 +93,32 @@ TEST(MessagesRequest, deserialize_summary_request) {
 	EXPECT_EQ("peng3", summary->addresses().at(2));
 	EXPECT_EQ("peng4", summary->addresses().at(3));
 
+}
+
+
+// Test that statistic message processing work
+TEST(MessagesRequest, deserialize_statistic_request) {
+	std::string bytes(statistic_message());
+
+	traffic::RequestMessage::ptr_t msg(
+			traffic::RequestMessage::parse_message(bytes.c_str(),
+							       bytes.size()));
+
+
+	ASSERT_EQ(typeid(traffic::StatisticRequest), typeid(*msg));
+
+	TypeVisitor v;
+	msg->accept(v);
+	ASSERT_EQ(TypeVisitor::STATISTIC, v.visited());
+
+	traffic::StatisticRequest* stat(dynamic_cast<traffic::StatisticRequest*>(msg.get()));
+
+	ASSERT_NE(nullptr, stat);
+	EXPECT_EQ(1, stat->range().start());
+	EXPECT_EQ(2, stat->range().end());
+
+	EXPECT_EQ("peng", stat->address());
+	EXPECT_EQ(traffic::StatisticRequest::HOUR, stat->interval());
 }
 
 
